6ec933ab44c173fbc879d0e65d9120013d88328d
anaco <p.m.yakushev@gmail.com>
2024-06-13 18:12:26 +0100

    added animations

--- player.py
+++ player.py
@@ -9,18 +9,26 @@ class Player(pg.sprite.Sprite):
         """Variable control"""
         super().__init__()
         sprite_sheet = SpriteSheet(sprite_sheet_path)
+        self.animation_len = 4
         self._load_images(sprite_sheet)
         self.image = self.walk_right[0]
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
+        self.last_update = 0
+        self.frame = 0
+        self.animation_cycle = self.walk_right
+        self.velocity = pg.Vector2(0, 0)
+
+
     def update(self):
         """Update control"""
         self._move()
+        self._animate(250)
 
     def _move(self):
         """Move control"""
-        self.velocity = pg.Vector2(0, 0)
+        self.velocity.update(0, 0)
         keys = pg.key.get_pressed()
 
         if keys[pg.K_w]:
@@ -37,8 +45,25 @@ class Player(pg.sprite.Sprite):
         self.rect.center += self.velocity
 
     def _load_images(self, sheet):
-        w, h = sheet.w // 4, sheet.h // 4
+        w, h = sheet.w // self.animation_len, sheet.h // self.animation_len
         self.walk_right = [sheet.get_image(i, h * 2, 32, 32) for i in range(0, w * 4, w)]
         self.walk_left = [sheet.get_image(i, h, 32, 32) for i in range(0, w * 4, w)]
         self.walk_up = [sheet.get_image(i, h * 3,  32, 32) for i in range(0, w * 4, w)]
         self.walk_down = [sheet.get_image(i, 0, 32, 32) for i in range(0, w * 4, w)]
+
+    def _animate(self, frame_len=100):
+        now = pg.time.get_ticks()
+        if now - self.last_update > frame_len and self.velocity.length() > 0:
+            self.last_update = now
+            if self.velocity.y > 0:
+                self.animation_cycle = self.walk_down
+            elif self.velocity.y < 0:
+                self.animation_cycle = self.walk_up
+            elif self.velocity.x > 0:
+                self.animation_cycle = self.walk_right
+            elif self.velocity.x < 0:
+                self.animation_cycle = self.walk_left
+
+            self.frame = (self.frame + 1) % self.animation_len
+
+            self.image = self.animation_cycle[self.frame]
