457e2f2b67dfda924ecdc25df2f326b5439f169f
anaco <p.m.yakushev@gmail.com>
2024-06-17 17:55:28 +0100

    something

--- main.py
+++ main.py
@@ -4,26 +4,44 @@ from utils import res
 from settings import *
 from player import Player
 
-pg.init()
-clock = pg.time.Clock()
-screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pg.display.set_caption(GAME_TITLE)
-pg.display.set_icon(pg.image.load(res / "sprite" / "frog.png"))
-player = Player(res / "sprite" / "player_sheet.png", (100, 100))
-all_sprites = pg.sprite.Group()
-all_sprites.add(player)
 
-running = True
-while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+class Game:
+    def __init__(self):
+        pg.init()
+        self.clock = pg.time.Clock()
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pg.display.set_caption(GAME_TITLE)
+        pg.display.set_icon(pg.image.load(res / "sprite" / "frog.png"))
+        self.running = True
 
-    all_sprites.update()
-    screen.fill((255, 255, 255))
-    all_sprites.draw(screen)
-    clock.tick(FPS)
-    pg.display.flip()
+    def new(self):
+        player = Player(res / "sprite" / "player_sheet.png", (100, 100))
+        self.all_sprites = pg.sprite.Group()
+        self.all_sprites.add(player)
+
+    def _events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+
+    def _update(self):
+        self.all_sprites.update()
+
+    def _draw(self):
+        self.screen.fill((255, 255, 255))
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def run(self):
+        while self.running:
+            self.clock.tick(FPS)
+            self._events()
+            self._update()
+            self._draw()
 
 
+if __name__ =="__main__":
 
+    game = Game()
+    game.new()
+    game.run()
