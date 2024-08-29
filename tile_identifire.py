6f4b3c49fff6ccaea080f319b6bd6792c55ba1e5
anaco <p.m.yakushev@gmail.com>
2024-08-29 17:49:03 +0100

    count the tiles

---
+++ tile_identifire.py
@@ -0,0 +1,30 @@
+import pygame as pg
+import pygame.freetype
+
+from settings import TILE_SIZE
+from utils import res
+
+w, h = 272, 128
+
+pg.init()
+screen = pg.display.set_mode((w * 2, h * 2))
+
+font = pg.freetype.Font(None, 16)
+image = pg.image.load(res / "map" / "2148910766900407bcfce9.26900631rpg_tileset (1).png")
+image = pg.transform.scale(image, (w * 2, h * 2))
+
+i = 0
+
+for y in range(0, h * 2, TILE_SIZE):
+    for x in range(0, w * 2, TILE_SIZE):
+        font.render_to(image, (x + 10, y + 10), str(i))
+        i += 1
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    screen.blit(image, (0, 0))
+    pg.display.flip()
