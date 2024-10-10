d27cea8e4330702adb630676b5686128af23d7c3
Gaggle-The-Creator <p.m.yakushev@gmail.com>
2024-10-10 17:14:44 +0100

    added missing files

---
+++ res/map/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="32" tileheight="32" tilecount="8" columns="4">
+ <image source="objects.png" width="128" height="64"/>
+</tileset>
